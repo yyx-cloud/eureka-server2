8cb8bb8dfd46a8a1e1e5a1dd6ea5b2549fe61509
yang_yunxiang <password>
2018-12-22 22:08:46 +0800

    init

---
+++ src/main/java/com/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: yunxiang.yang
+ * @Date: 2018/12/20 18:08
+ */
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class);
+    }
+}
